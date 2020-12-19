4c615cb46c99560dd36262e9f04f6568b71508ae
sheylaninda <48579832+sheylaninda@users.noreply.github.com>
2020-12-19 18:52:45 +0800

    Add files via upload

---
+++ SISWA/src/koneksi/Koneksi.java
@@ -0,0 +1,22 @@
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class Koneksi {
+    Connection koneksi;
+    
+    public static Connection Koneksi() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection koneksi = DriverManager.getConnection("jdbc:mysql://localhost/siswa", "root", "");
+            
+            return koneksi;
+        } catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            
+            return null;
+        }
+    }
+}
